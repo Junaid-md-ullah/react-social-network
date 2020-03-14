77b994073e6289d197265bb54b81925a5e59eac2
Junaid-md-ullah <junaid00.ju@gmail.com>
2020-03-14 14:22:39 +0600

    all the requirements done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 const Cart = (props) => {
-    console.log(props.people)
+    const cart=props.cart;
+    let total=0;
+    for(let i=0;i<cart.length;i++){
+        const people=cart[i];
+        total=total+people.salary;
+    }
+    let fund=0;
+    let totalFund=0;
+    for(let i=0;i<cart.length;i++){
+        const people=cart[i];
+        fund=people.salary*0.02;
+        totalFund=totalFund+fund;
+    }
+    
+
     return (
         <div>
             <h3>Social Status</h3> 
             <h5>Total Neighbours: {props.cart.length}</h5>
-            <h4>Neighbours Total Monthly Income:{0}</h4>
-            <h3>Social Fund Raised: {0}</h3>
+            <h4>Neighbours Total Monthly Income:{total}</h4>
+            <h3>Social Fund-Raised: {totalFund}</h3>
         </div>
     );
 };
--- src/components/EachPeople/EachPeople.js
+++ src/components/EachPeople/EachPeople.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import './EachPeople.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus, faUserCheck,faEnvelope,faMoneyBill} from '@fortawesome/free-solid-svg-icons'
 const EachPeople = (props) => {
 
     const {name,email,img,salary}=props.people;
     const [isAdd,setAdd]= useState(false);
-    let addButton=<button onClick={()=>{props.handleAddPeople(props.people); setAdd(true)}}>Add Me</button>
+    let addButton=<button onClick={()=>{props.handleAddPeople(props.people); setAdd(true)}}><FontAwesomeIcon icon={faUserPlus} /> Add Me</button>
     if(isAdd){
-        addButton=<button className="added" disabled>Added</button>
+        addButton=<button className="added" disabled><FontAwesomeIcon icon={faUserCheck} /> Added</button>
     }
     return (
         <div className="each-people">
@@ -16,8 +17,8 @@ const EachPeople = (props) => {
             </div>
             <div className="people-details">
                 <h5>{name}</h5>
-                <p><small>Email: {email}</small></p>
-                <p>${salary}</p>
+                <p><small><FontAwesomeIcon icon={faEnvelope} /> {email}</small></p>
+                <p><FontAwesomeIcon icon={faMoneyBill} /> ${salary}</p>
                 {addButton}
             </div>
                 
